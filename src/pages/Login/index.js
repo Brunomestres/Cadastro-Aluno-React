f128c035a2613f78a848f3e6b53da6ba2e30066b
Bruno <brunomestres@gmail.com>
2021-02-12 18:32:31 -0300

    inserindo styled conponents

---
+++ src/pages/Login/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Title } from './styled';
+
+export default function Login() {
+  return (
+    <>
+      <Title isRed>
+        Login
+        <small>Flw</small>
+      </Title>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        inventore nulla distinctio voluptate. Explicabo, tenetur autem. Officia
+        laborum amet earum deserunt aspernatur in voluptas. Voluptate quis aut
+        libero iure corporis!
+      </p>
+    </>
+  );
+}
